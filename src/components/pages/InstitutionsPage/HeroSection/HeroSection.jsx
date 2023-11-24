c967d857c589e62cd2969ff87a7be97670881ca5
YanaLiubchenko <yana.lyubchenko@dotcode.pro>
2023-11-24 13:48:30 +0200

    animated chart

--- src/components/pages/InstitutionsPage/HeroSection/HeroSection.jsx
+++ src/components/pages/InstitutionsPage/HeroSection/HeroSection.jsx
@@ -1,8 +1,11 @@
+'use client';
+
 import content from '@/asset/content.json';
 import Image from 'next/image';
 
 import BRButton from '@/components/BRButton/BRButton';
 import { BRTypography } from '@/components/BRTypography';
+import { AnimatedChart } from '@/components/pages/InstitutionsPage/AnimatedChart';
 
 import heroImage from '../../../../../public/asset/img/hero_inst.png';
 import styles from './heroSection.module.css';
@@ -16,24 +19,27 @@ export const HeroSection = () => {
 
   return (
     <div className={styles.heroSectionWrapper}>
-      <div className={styles.imgWrapper}>
-        <Image
-          src={heroImage}
-          alt="Main image"
-          loading="lazy"
-          placeholder="blur"
-          blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/wkAAvYB8z4HYOYAAAAASUVORK5CYII="
-        />
-      </div>
-      <div className={styles.textWrapper}>
-        <BRTypography text={title} variantMapping="headlineHero" />
-        <BRTypography text={description} variantMapping="bodyM" />
+      <div className={styles.heroContent}>
+        <div className={styles.imgWrapper}>
+          <Image
+            src={heroImage}
+            alt="Main image"
+            loading="lazy"
+            placeholder="blur"
+            blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/wkAAvYB8z4HYOYAAAAASUVORK5CYII="
+          />
+        </div>
+        <div className={styles.textWrapper}>
+          <BRTypography text={title} variantMapping="headlineHero" />
+          <BRTypography text={description} variantMapping="bodyM" />
 
-        <div className={styles.buttonsWrapper}>
-          <BRButton text={leftButton} variant="contained" />
-          <BRButton text={rightButton} variant="outlined" />
+          <div className={styles.buttonsWrapper}>
+            <BRButton text={leftButton} variant="contained" />
+            <BRButton text={rightButton} variant="outlined" />
+          </div>
         </div>
       </div>
+      <AnimatedChart />
     </div>
   );
 };
