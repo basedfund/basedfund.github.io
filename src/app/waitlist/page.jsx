45fe80b943fc39ecc27824218893b6f14fc454cb
YanaLiubchenko <yana.lyubchenko@dotcode.pro>
2023-11-30 13:24:11 +0200

    form

---
+++ src/app/waitlist/page.jsx
@@ -0,0 +1,9 @@
+import { WaitlistPage } from '@/components/pages/WaitlistPage';
+
+export const metadata = {
+  title: 'Join Waitlist',
+};
+
+export default function Waitlist() {
+  return <WaitlistPage />;
+}
